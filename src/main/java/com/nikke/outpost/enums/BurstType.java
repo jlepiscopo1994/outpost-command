9d5f40d45aa68840dfc88f17f70c8776e57df3a5
jlepiscopo1994 <joeycentral@gmail.com>
2026-08-07 16:17:41 -0400

    feat(domain): implement core gameplay enums (Closes #1)

---
+++ src/main/java/com/nikke/outpost/enums/BurstType.java
@@ -0,0 +1,7 @@
+package com.nikke.outpost.enums;
+
+public enum BurstType {
+    BURST_1,
+    BURST_2,
+    BURST_3
+}
---
+++ src/main/java/com/nikke/outpost/enums/ClassType.java
@@ -0,0 +1,7 @@
+package com.nikke.outpost.enums;
+
+public enum ClassType {
+    ATTACKER,
+    DEFENDER,
+    SUPPORTER
+}
